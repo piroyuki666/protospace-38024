b1fb151338b1139a1d8505cd9e217943d853dfbd
piroyuki666 <shtr3636@gmail.com>
2022-05-19 16:32:04 +0900

    データベースマイグレーションの修正

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,10 @@ ActiveRecord::Schema.define(version: 2022_05_18_012830) do
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,4 +72,5 @@ ActiveRecord::Schema.define(version: 2022_05_18_012830) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
